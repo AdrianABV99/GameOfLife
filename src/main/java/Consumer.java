5290500101c10f085a140bb2703fe7b4dfab8a2c
Adrian <adrian99achim@gmail.com>
2021-12-17 12:06:45 +0200

    Added implementation for Consumer

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,2 +1,64 @@
-public class Consumer {
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer<L extends Number, S> {
+
+    private final static String TOPIC = "app-topic";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+    final String consumerGroupID = "java-Group-consumer";
+
+
+    private static KafkaConsumer<Integer, String> createConsumer() {
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                "KafkaExampleConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                IntegerDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+
+        // Create the consumer using props.
+        KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
+
+        // Subscribe to the topic.
+        consumer.subscribe(Collections.singletonList(TOPIC));
+        return consumer;
+    }
+
+
+    static void runConsumer() throws InterruptedException {
+        final KafkaConsumer<Integer, String> consumer = createConsumer();
+
+        final int giveUp = 100;   int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Integer, String> consumerRecords =
+                    consumer.poll(Duration.ofMillis(1000));
+
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) break;
+                else continue;
+            }
+
+            consumerRecords.forEach(record -> {
+                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
+                        record.key(), record.value(),
+                        record.partition(), record.offset());
+            });
+
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+    }
 }
